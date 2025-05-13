44cbc75fa7182d6e8ccfbf24453f98611820f28a
Albert Ceballos <albert.ceballos@somosinapsis.com>
2025-05-13 17:33:51 +0200

    sync

--- generate_all_jobs.py
+++ generate_all_jobs.py
@@ -2,17 +2,19 @@ import argparse
 import random
 from subprocess import call
 
-LR_OPTIONS = [1e-4, 5e-4, 1e-3]
-COARSE_SAMPLES_OPTIONS = [32, 64, 96]
-IMPORTANCE_SAMPLES_OPTIONS = [32, 64, 128]
-TEMPORAL_DISTORTION_OPTIONS = [True, False]
+LR_OPTIONS = [1e-4, 1e-3]
+COARSE_SAMPLES_OPTIONS = [32, 96]
+NUM_RAYS_PER_BATCH_OPTIONS = [1024, 8192, 16384]
+NUM_STEPS = [10000, 60000, 200000]
 
-def call_train_script(dataset, model, data_type, lr, coarse_samples, importance_samples, temporal_distortion):
+def call_train_script(dataset, model, data_type, lr, coarse_samples, num_rays_per_batch, num_iterations):
     extra_args = (
         f"--optimizers.fields.optimizer.lr {lr} "
         f"--pipeline.model.num-coarse-samples {coarse_samples} "
-        f"--pipeline.model.num-importance-samples {importance_samples} "
-        f"--pipeline.model.enable-temporal-distortion {temporal_distortion}"
+        #f"--pipeline.model.num-importance-samples {importance_samples} "
+        f"--pipeline.datamanager.train-num-rays-per-batch {num_rays_per_batch} "
+        f"--max-num-iterations {num_iterations} "
+        f""
     )
 
     command = [
@@ -32,28 +34,40 @@ def main():
     parser.add_argument("--dataset", required=True, help="El dataset a usar para el entrenamiento")
     parser.add_argument("--model", required=True, help="El modelo NeRF a usar para el entrenamiento")
     parser.add_argument("--data-type", choices=["dnerf", ""], default="dnerf", help="Tipo de datos")
+    parser.add_argument("--search", type=int, default=0, help="0: Random Search, 1: Grid Search (default: 0)")
     parser.add_argument("--samples", type=int, default=10, help="Número de combinaciones aleatorias a generar")
     args = parser.parse_args()
 
-    used_combinations = set()
     i = 0
-
-    while i < args.samples:
-        combo = (
-            random.choice(LR_OPTIONS),
-            random.choice(COARSE_SAMPLES_OPTIONS),
-            random.choice(IMPORTANCE_SAMPLES_OPTIONS),
-            random.choice(TEMPORAL_DISTORTION_OPTIONS),
-        )
-        # Evitar duplicados
-        if combo in used_combinations:
-            continue
-        used_combinations.add(combo)
-        i += 1
-        num_samples, max_iters, cones, rays_batch = combo
-        print(f"\nGenerando entrenamiento {i} con parámetros:")
-        print(f"num_samples={num_samples}, max_iters={max_iters}, cones={cones}, rays_batch={rays_batch}")
-        call_train_script(args.dataset, args.model, args.data_type, num_samples, max_iters, cones, rays_batch)
+    used_combinations = set()
+    if args.search == 1:
+        # Generar combinaciones de parámetros para Grid Search
+        for lr in LR_OPTIONS:
+            for coarse_samples in COARSE_SAMPLES_OPTIONS:
+                for num_rays_per_batch in NUM_RAYS_PER_BATCH_OPTIONS:
+                    for num_steps in NUM_STEPS:
+                        combo = (lr, coarse_samples, num_rays_per_batch, num_steps)
+                        print(f"\nGenerando entrenamiento {i} con parámetros:")
+                        print(f"lr={lr}, coarse_samples={coarse_samples} rays_batch={num_rays_per_batch}, num_steps={num_steps}")
+                        call_train_script(args.dataset, args.model, args.data_type, lr, coarse_samples, num_rays_per_batch, num_steps)
+                        i += 1
+    else:
+        while i < args.samples:
+            combo = (
+                random.choice(LR_OPTIONS),
+                random.choice(COARSE_SAMPLES_OPTIONS),
+                random.choice(NUM_RAYS_PER_BATCH_OPTIONS),
+                random.choice(NUM_STEPS)
+            )
+            # Evitar duplicados
+            if combo in used_combinations:
+                continue
+            used_combinations.add(combo)
+            i += 1
+            combo = (lr, coarse_samples, num_rays_per_batch, num_steps)
+            print(f"\nGenerando entrenamiento {i} con parámetros:")
+            print(f"lr={lr}, num_samples={coarse_samples} rays_batch={num_rays_per_batch}, num_steps={num_steps}")
+            call_train_script(args.dataset, args.model, args.data_type, lr, coarse_samples, num_rays_per_batch, num_steps)
 
 if __name__ == "__main__":
     main()
